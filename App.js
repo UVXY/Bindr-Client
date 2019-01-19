eec865f6c7b17728ce49cc34c51c99eb29332ffa
dev-mev <40147757+dev-mev@users.noreply.github.com>
2019-01-18 20:58:05 -0800

    add config for android emulator - add .gitignore - minor UI updates to myfavorites to possibly make that the user profile page

--- App.js
+++ App.js
@@ -33,13 +33,14 @@ export default class App extends React.Component {
         require('./assets/images/robot-dev.png'),
         require('./assets/images/robot-prod.png'),
       ]),
-      // Font.loadAsync({
-      //   // This is the font that we are using for our tab bar
-      //   ...Icon.Ionicons.font,
-      //   // We include SpaceMono because we use it in HomeScreen.js. Feel free
-      //   // to remove this if you are not using it in your app
-      //   'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      // }),
+      Font.loadAsync({
+        // This is the font that we are using for our tab bar
+        ...Icon.Ionicons.font,
+        // We include SpaceMono because we use it in HomeScreen.js. Feel free
+        // to remove this if you are not using it in your app
+        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+        'Roboto_medium': require('./node_modules/native-base/Fonts/Roboto_medium.ttf')
+      }),
     ]);
   };
 
--- utils/API.js
+++ utils/API.js
@@ -1,22 +1,25 @@
 import axios from "axios";
+import * as config from "../config.json";
+
+const baseUrl = config.API_URL;
 
 export default {
     login: (userObj) => {
-        return axios.post("http://localhost:3001/auth/login", userObj)
+        return axios.post(`${baseUrl}/auth/login`, userObj)
     },
     logout: () => {
-        return axios.post("http://localhost:3001/auth/logout")
+        return axios.post(`${baseUrl}/auth/logout`)
     },
     getUser: () => {
-        return axios.get("http://localhost:3001/auth/user")
+        return axios.get(`${baseUrl}/auth/user`)
     },
     registerUser: (userObj) => {
-        return axios.post("http://localhost:3001/auth/signup", userObj)
+        return axios.post(`${baseUrl}/auth/signup`, userObj)
     },
     saveBook: (bookObj, userId) => {
-        return axios.post("http://localhost:3001/api/book/" + userId, bookObj)
+        return axios.post(`${baseUrl}/api/book/` + userId, bookObj)
     },
     getUserBooks: (userId) => {
-        return axios.get("http://localhost:3001/api/book/" + userId)
+        return axios.get(`${baseUrl}/api/book/` + userId)
     }
 }
