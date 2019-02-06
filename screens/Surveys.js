ff4957c3a67d058879274dfbea259a3bbabfe045
ricky <ricky@rickys-MacBook-Air.local>
2019-02-06 09:50:47 -0800

    Cleaned up previous code leftover

--- screens/Surveys.js
+++ screens/Surveys.js
@@ -4,8 +4,6 @@ import { View } from 'native-base';
 import { StyleSheet, ScrollView } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import PicturePicker from '../components/PicturePicker';
-import ImageCard from '../components/DataCard';
-import Roulette from '../components/Roulette';
 import Wordsearch from '../components/WordsearchBoard';
 import API from '../utils/API';
 import Header from '../components/Header';
@@ -16,26 +14,9 @@ export default class CardShowcaseExample extends Component {
   };
 
   state = {
-    images: [],
     tags:[],
-    gifSearch: "",
     user: null
   }
-
-  gifArr = [
-    "travel", "wandering", "lost", "best", "earliest-list", "favourites", "writer", "sad", "crying", "death", "learning", "technology", "help", "comedy", "funny", "humor", "humour", "satire", "old", "ancient", "storm", "cloudy", "celebrity", "movies", "blond", "fantasy", "sci-fi", "science-fiction", "sf", "classics", "business", "career", "creativity", "fitness", "happiness", "health", "love", "non-fiction", "nonfiction", "productivity", "relationships", "romance", "self-help", "success", "wellness", "baseball", "sports", "book club", "historical", "literary", "summer", "sunny", "clear", "warm", "autumn", "books", "coffee", "creep", "creepy", "dark", "fall", "fireplace", "freaky", "halloween", "leaves", "november", "october", "pumpkin", "rain", "rainy", "reading", "scary", "september", "spooky", "sweater", "tea", "thanksgiving", "intrigue", "mystery", "thriller", "fiction", "seasons", "setting", "weather", "winter", "cold", "warmup"
-  ]
-  NumberList(props) {
-  const numbers = props.numbers;
-  const listItems = numbers.map((number) =>
-    <li key={number.toString()}>
-      {number}
-    </li>
-  );
-  return (
-    <ul>{listItems}</ul>
-  );
-}
 
  
   handleGifPress = (search) => {
